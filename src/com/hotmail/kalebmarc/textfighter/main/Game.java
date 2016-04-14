a78a6dbee70f70420b0b052dd8cacf2fc5f556f9
Kaleb Haslam <kalebmarc@hotmail.com>
2016-04-13 21:20:42 -0700

    Fixed merge conflict (For real this time; forgot I had un-commit changes)

--- src/com/hotmail/kalebmarc/textfighter/main/Game.java
+++ src/com/hotmail/kalebmarc/textfighter/main/Game.java
@@ -59,13 +59,16 @@ public class Game {
 		Ui.println("| 2) No, Start a new game                   |");
 		Ui.println("|___________________________________________|");
 
-		switch (Action.getValidInt()) {
+		int choice = Action.getValidInt();
+		switch(choice){
 			case 1:
-				if (SaveAndLoad.load()) break;
+				if(SaveAndLoad.load()) break;
 			default:
 				setDif(getDifficulty(), true, false);
 				Health.set(100, 100);
 				Enemy.encounterNew();
+				if(choice != 1)
+					User.promptNameSelection();
 				break;
 		}
 
@@ -93,7 +96,7 @@ public class Game {
 			Ui.println("     Level " + Xp.getLevel() + "      " + Xp.getFull());
 			Ui.println("     Kill Streak: " + Stats.kills);
 			Ui.println("     Highest Kill Streak: " + Stats.highScore);
-			Ui.println("--Player Info--");
+			Ui.println("--" + User.name() + "--");
 			Ui.println("     Health: " + getStr());
 			Ui.println("     Coins: " + Coins.get());
 			Ui.println("     First-Aid kits: " + FirstAid.get());
