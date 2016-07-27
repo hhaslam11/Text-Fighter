6963c3e820feec60504bbd9204e6360aa8348955
kaleb <kalebmarc@hotmail.com>
2016-07-26 19:46:15 -0700

    Reverted version.java back

--- src/com/hotmail/kalebmarc/textfighter/main/Version.java
+++ src/com/hotmail/kalebmarc/textfighter/main/Version.java
@@ -2,27 +2,34 @@ package com.hotmail.kalebmarc.textfighter.main;
 
 class Version {
 	private Version(){}
-	
-	private static final String VERSION = "4.7.1";
-	
+
+	private static final String VERSION = "4.8DEV";
+
 	private static final String STAGE = "Alpha";
-	
-	private static final String DESC = ""
+
+	private static final String DESC = "" //Award for "worse game description" goes to:
+			//But seriously; add information that the user actually cares about
 			+ "Text-Fighter is a Text-Based\n"
 			+ "Fighter RPG game, completely\n"
-			+ "written in Java. Text-Fighter\n"
-			+ "is made by Kaleb Haslam\n\n"
+			+ "written in Java.\n\n"
 			+ "Text-Fighter is currently in Alpha stage\n"
 			+ "which means it's still in early development,\n"
 			+ "and will contain lots of bugs and missing features.";
-	
+
 	private static final String CHANGE_LOG = ""
-			+ "(Compatible with previous saves)\n\n"
+			+ "(Not compatible with previous saves)\n\n"
+
+			+ "New Stuff:\n"
+			+ "-\n"
+			+ "-\n"
+			+ "-\n"
+			+ "-\n"
+			+ "-\n\n"
 
 			+ "Bug Fixes:\n"
-			+ "- Player takes twice as much damage than supposed to\n"
-			+ "- Issue with buying certain weapons, buying ammo and equipping certain weapons";
-	
+			+ "-\n"
+			+ " ";
+
 	public static String get(){
 		return VERSION;
 	}
