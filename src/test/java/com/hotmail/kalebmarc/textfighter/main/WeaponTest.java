9620915a5dea5b2222a7580e50bcdf18b58a246a
Alex Puga <alex.puga@wsu.edu>
2020-10-24 16:25:48 -0700

    initiated tests for weapon changes

---
+++ src/test/java/com/hotmail/kalebmarc/textfighter/main/WeaponTest.java
@@ -0,0 +1,34 @@
+package com.hotmail.kalebmarc.textfighter.main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeaponTest {
+
+    @Before
+    public void setUp() throws Exception {
+        
+    }
+
+    @Test
+    public void dealDam() {
+    }
+
+    @Test
+    public void setCriticalChance() {
+    }
+
+    @Test
+    public void getCriticalChance() {
+    }
+
+    @Test
+    public void displayDamageDealt() {
+    }
+
+    @Test
+    public void didCriticalHit() {
+    }
+}
