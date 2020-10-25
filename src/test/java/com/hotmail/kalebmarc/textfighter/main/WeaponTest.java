5ee17781851d3c70f3dae9c90ed6746969a77997
Alex Puga <alex.puga@wsu.edu>
2020-10-24 17:56:30 -0700

    finished test cases for CriticalHits

--- src/test/java/com/hotmail/kalebmarc/textfighter/main/WeaponTest.java
+++ src/test/java/com/hotmail/kalebmarc/textfighter/main/WeaponTest.java
@@ -1,5 +1,9 @@
 package com.hotmail.kalebmarc.textfighter.main;
 
+import com.hotmail.kalebmarc.textfighter.player.Stats;
+import com.hotmail.kalebmarc.textfighter.main.Enemy;
+
+import com.hotmail.kalebmarc.textfighter.player.Xp;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,18 +12,42 @@ import static org.junit.Assert.*;
 public class WeaponTest {
     Weapon rifle;
     Weapon pistol;
+    Enemy enemy;
 
     @Before
     public void setUp() throws Exception {
-        rifle = new Weapon("Rifle", 1, 18, true, 275, 1, 5, 10, true, true , 1);
-        rifle.setAmmo(10, true);
-        pistol = new Weapon("Pistol", 1, 18, true, 250, 1, 4, 15, true, true);
-        pistol.setAmmo(10, true);
+        rifle = new Weapon("Rifle", 1, 18, true, 275, 1, 5, 0, true, true , 1);
+        rifle.setAmmo(10,true);
+        pistol = new Weapon("Pistol", 1, 18, true, 250, 1, 4, 0, true, true);
+        pistol.setAmmo(10,true);
+        enemy = new Enemy("Dark Elf", 45, 10, 15, 10, 15, 15, true, false);
+        Enemy.encounterNew();
+        Weapon.BULLET_DAMAGE = 10;
+
     }
 
     @Test
     public void dealDam() {
+        /*
+        * Make sure to click ok pop ups
+        * */
+        // Non critical
+        int enemyStartHealth = enemy.getHealth();
+        assertEquals(enemy.getHealth(), 45);
+        // Make sure there is 0 chance of critical damage
+        pistol.setCriticalChance(0);
+        pistol.dealDam();
+        assertNotEquals(enemyStartHealth, enemy.getHealth());
+        assertEquals(enemyStartHealth - Weapon.BULLET_DAMAGE , enemy.getHealth());
+        int enemyAfterHealth = enemy.getHealth();
+        // Reset pistol's criticalChance back to default
+        pistol.setCriticalChance(.01);
 
+        // Critical
+        rifle.dealDam();
+        assertNotEquals(enemyAfterHealth, enemy.getHealth());
+        // Enemy died and gets respawned immediately. Health should be 45 or whatever test enemy health set to.
+        assertEquals(enemyStartHealth , enemy.getHealth());
     }
 
     @Test
@@ -27,7 +55,7 @@ public class WeaponTest {
         // Check the Rifle critical chance is set to 1
         assertTrue(rifle.getCriticalChance() == 1);
         rifle.setCriticalChance(-200);
-        assertTrue(rifle.getCriticalChance() == -200);
+        assertTrue(rifle.getCriticalChance() == .01);
         rifle.setCriticalChance(1);
         assertTrue(rifle.getCriticalChance() == 1);
 
@@ -53,9 +81,35 @@ public class WeaponTest {
 
     @Test
     public void displayDamageDealt() {
+        int nDealt = Stats.totalDamageDealt;
+        int xp = Xp.getBattleXp();
+
+        assertEquals(nDealt,Stats.totalDamageDealt);
+        assertEquals(xp, Xp.getBattleXp());
+
+        pistol.dealDam();
+
+        assertNotEquals(nDealt,Stats.totalDamageDealt);
+        assertNotEquals(xp, Xp.getBattleXp());
+
+        // Set to current values
+        nDealt = Stats.totalDamageDealt;
+        xp = Xp.getBattleXp();
+
+        // Sets pistols ammo to 0 so cant do damage
+        pistol.setAmmo(-pistol.getAmmo(), true);
+        assertEquals(nDealt,Stats.totalDamageDealt);
+        assertEquals(xp, Xp.getBattleXp());
+
     }
 
     @Test
     public void didCriticalHit() {
+        assertEquals(rifle.didCriticalHit(), true);
+        rifle.setCriticalChance(0);
+        assertFalse(rifle.didCriticalHit());
+        rifle.setCriticalChance(1);
+        assertTrue(rifle.didCriticalHit());
+
     }
 }
